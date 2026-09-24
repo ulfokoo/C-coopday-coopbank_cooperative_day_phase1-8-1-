5f3a9eca297c8808a4a51c7a94e4e9c26914f12c
ulfokoo <ulfata.zawude@coopbankoromiasc.com>
2026-09-24 15:38:25 +0300

    Fix indentation in activities routes

--- app/activities/routes.py
+++ app/activities/routes.py
@@ -1,4 +1,4 @@
-  from flask import Blueprint, render_template, redirect, url_for, flash, request, abort
+from flask import Blueprint, render_template, redirect, url_for, flash, request, abort
 from flask_login import login_required, current_user
 
 from app.extensions import db
